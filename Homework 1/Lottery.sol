abde1b02795e5da02feada5e62a58b71df396335
Kadir Elmacı <abdulkadirelmaci@gmail.com>
2022-04-27 20:29:00 +0300

    Add some stuff

--- Homework 1/Lottery.sol
+++ Homework 1/Lottery.sol
@@ -5,8 +5,12 @@ import "./TicketNFT.sol";
 import "./ILottery.sol";
 
 contract Lottery is ILottery {
+    uint price = 10;
     mapping(address => uint256) balances;
-    mapping(uint => uint256) totalMoneyInLotteries;
+    mapping(uint => uint) totalMoneyInLotteries;
+    mapping(uint => mapping(address => uint)) lastOwnedTicketIndices; //LotteryNo => Owner => Ticket Index
+    mapping(uint => mapping(address => mapping(uint => uint))) ownedTickets; //LotteryNo => Owner => Ith Ticket => Ticket No
+    mapping(uint => mapping(uint => uint)) winningTickets; //LotteryNo => Ith Ticket => Ticket No
     uint _initialTimeInWeeks;
     TLToken _tokenContract;
     TicketNFT _ticketContract;
@@ -14,7 +18,7 @@ contract Lottery is ILottery {
     constructor(address tokenAddress, address nftAddress) {
         _tokenContract = TLToken(tokenAddress);
         _ticketContract = TicketNFT(nftAddress);
-        _initialTimeInWeeks = now;
+        _initialTimeInWeeks = block.timestamp;
     }
     
     function depositTL(uint amnt) public {
@@ -23,11 +27,16 @@ contract Lottery is ILottery {
     }
     
     function withdrawTL(uint amnt) public {
-        _tokenContract.decreaseAllowance(msg.sender, amnt);
+        _tokenContract.increaseAllowance(msg.sender, amnt);
+        balances[msg.sender] -= amnt;
     }
 
     function buyTicket(bytes32 hash_rnd_number) public {
-
+        require(balances[msg.sender] >= price, "Insufficient funds");
+        balances[msg.sender] -= price;
+        uint newIndex = lastOwnedTicketIndices[lottery_no][msg.sender] + 1;
+        lastOwnedTicketIndices[lottery_no][msg.sender] = newIndex;
+        ownedTickets[lottery_no][msg.sender][newIndex] = uint(hash_rnd_number);
     }
 
     function collectTicketRefund(uint ticket_no) public {
@@ -39,11 +48,12 @@ contract Lottery is ILottery {
     }
     
     function getLastOwnedTicketNo(uint lottery_no) public view returns(uint,uint8 status) {
-
+        uint index = lastOwnedTicketIndices[lottery_no][msg.sender];
+        return ownedTickets[lottery_no][msg.sender][index];
     }
 
     function getIthOwnedTicketNo(uint i,uint lottery_no) public view returns(uint,uint8 status) {
-
+        return winningTickets[i];
     }
 
     function checkIfTicketWon(uint ticket_no) public view returns (uint amount) {
@@ -55,7 +65,7 @@ contract Lottery is ILottery {
     }
 
     function getIthWinningTicket(uint i, uint lottery_no) public view returns (uint ticket_no,uint amount) {
-
+        
     }
 
     function getLotteryNo(uint unixtimeinweek) public view returns (uint lottery_no) {
