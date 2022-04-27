d1da76b2b449d7458fe5ff912b8b3e42554f198d
Kadir Elmacı <abdulkadirelmaci@gmail.com>
2022-04-27 19:48:56 +0300

    Use open zeppelin contracts

--- Homework 1/TicketNFT.sol
+++ Homework 1/TicketNFT.sol
@@ -1,28 +1,8 @@
 pragma solidity ^0.8.0;
 
-import "./ITicketNFT.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TicketNFT is ERC721 {
-
-    mapping (address=>uint256) balances;
-    mapping (address=>uint256) tokens;
-    mapping (uint256=>address) owners;
-
-    function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes memory data) external payable {
-        require(owners[_tokenId] != _from, "You are not the owner of the token");
-        require(_to == address(0), "0 address is not valid");
-    }
-
-    function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable {
-        safeTransferFrom(_from, _to, _tokenId, "");
-    }
-
-
-    function balanceOf(address _owner) external view returns (uint256 balance){
-        return balances[_owner];
-    }
-
-    function ownerOf(uint256 _tokenId) external view returns (address){
-        return owners[_tokenId];
+    constructor() ERC721("Tickets", "TKT") {
     }
 }
