f9de92bf5ada4d09c65b26250178441b27a09931
Kadir Elmacı <abdulkadirelmaci@gmail.com>
2022-05-06 22:26:31 +0300

    Add mint

--- Homework 1/TicketNFT.sol
+++ Homework 1/TicketNFT.sol
@@ -5,4 +5,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract TicketNFT is ERC721 {
     constructor() ERC721("Tickets", "TKT") {
     }
+
+     function mint(address owner, uint256 tokenId) external {
+        _safeMint(owner, tokenId);
+    }
 }
