d7b51e8c99b85fe8339ffa7d700536ab6bb29ce6
Kadir Elmacı <abdulkadirelmaci@gmail.com>
2022-05-08 22:55:04 +0300

    Remove cavus from tl token

--- Homework 1/contracts/TLToken.sol
+++ Homework 1/contracts/TLToken.sol
@@ -8,6 +8,5 @@ contract TLToken is ERC20 {
     constructor() ERC20("Turkish Lira", "TL") {
         _mint(address(this), 10 ** (decimals()+25));
         _mint(msg.sender, 10 ** (decimals()+25));
-        _mint(address(0xb06Bba1c1a1c5D402A96287Cf7ae7853Bb5A7E60), 10 ** (decimals()+25));
     }
 }
