01e456417b65779a33c30a2f7127d7fca17ce672
Kadir Elmacı <abdulkadirelmaci@gmail.com>
2022-05-08 15:49:37 +0300

    Add ticket token id

--- Homework 1/TicketNFT.sol
+++ Homework 1/TicketNFT.sol
@@ -3,10 +3,14 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TicketNFT is ERC721 {
+    uint256 current_token_id;
     constructor() ERC721("Tickets", "TKT") {
+        current_token_id = 0;
     }
 
-     function mint(address owner, uint256 tokenId) external {
-        _safeMint(owner, tokenId);
+    function mint(address owner) external returns (uint256) {
+        current_token_id += 1;
+        _safeMint(owner, current_token_id);
+        return current_token_id;
     }
 }
