83786b897e15344ae2e3a35193b9bd9589a4594d
Kadir Elmacı <abdulkadirelmaci@gmail.com>
2022-05-08 22:20:43 +0300

    Change port

--- Homework 1/truffle-config.js
+++ Homework 1/truffle-config.js
@@ -43,16 +43,16 @@ module.exports = {
     //
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
-    },
-    ropsten: {
-        provider: function() {
-          return new HDWalletProvider(MNEMONIC, "https://core.bloxberg.org/RPC/YOUR_API_KEY")
-        },
-        network_id: 3,
-        gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
     }
+    // ropsten: {
+    //     provider: function() {
+    //       return new HDWalletProvider(MNEMONIC, "https://core.bloxberg.org/RPC/YOUR_API_KEY")
+    //     },
+    //     network_id: 3,
+    //     gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
+    // }
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
