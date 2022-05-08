4d2f558dabe9bdd2d1997645677fc23236a8196e
Kadir Elmacı <abdulkadirelmaci@gmail.com>
2022-05-08 15:49:46 +0300

    Add stuff

--- Homework 1/Lottery.sol
+++ Homework 1/Lottery.sol
@@ -5,23 +5,30 @@ import "./TicketNFT.sol";
 import "./ILottery.sol";
 
 contract Lottery is ILottery {
-    enum Status {BOUGHT, REVEALED, WON, REFUNDED}
-
-    struct Ticket {
-        address owner;
-        bytes32 random_hash;
-        Status status;// 0 not revealed, 1 revelead, 2 won, 3 refunded
+    enum Status {
+        BOUGHT, 
+        REVEALED, 
+        WON, 
+        REFUNDED
     }
 
+    struct Ticket {
+        uint lottery_no;
+        address owner;
+        bytes32 random_hash;
+        Status status;// 0 BOUGHT, 1 revelead, 2 won, 3 refunded
+    }
 
     uint price = 10;
-    uint256 current_ticket_no = 0;
+
     mapping(address => uint256) balances;
     mapping(uint => uint) totalMoneyInLotteries;
     mapping(uint => mapping(address => uint[])) ownedTickets; //LotteryNo => Owner => Ticket No[]
     mapping(uint => uint[]) lotteryTickets;
-    mapping(uint => mapping(address => uint[])) revealedTickets; //Is this necessary after holding every ticket in lottery tickets
-    mapping(uint => mapping(uint => Ticket)) ticketNoTickets; // LotteryNo => Ticket no => ticket struct
+    mapping(uint => uint[]) revealedTickets; //Is this necessary after holding every ticket in lottery tickets
+    mapping(uint => uint[]) winningTickets; //Is this necessary after holding every ticket in lottery tickets
+    mapping(uint => uint) xorOfLotteries; // LotteryNo => (Xor result of that lottery)
+    mapping(uint => Ticket) ticketNoTickets; //Ticket no => ticket struct
 
     uint _initialTime;
     TLToken _tokenContract;
@@ -49,44 +56,55 @@ contract Lottery is ILottery {
 
     function buyTicket(bytes32 hash_rnd_number) public {
         require(balances[msg.sender] >= price, "Insufficient funds");
+        require(isInPurchase(), "Not in purchase");
         balances[msg.sender] -= price;
         _tokenContract.decreaseAllowance(msg.sender, price);
         uint lottery_no = getLotteryNo(block.timestamp);
 
-        //Is mint no correct? Use the current ticket no or smthng else
-        _ticketContract.mint(msg.sender, uint256(hash_rnd_number));
-        ownedTickets[lottery_no][msg.sender].push(current_ticket_no);
-        lotteryTickets[lottery_no].push(current_ticket_no);
-        ticketNoTickets[lottery_no][current_ticket_no] = Ticket({owner: msg.sender, random_hash: hash_rnd_number, status: Status.BOUGHT});
+        //TODO Is mint no correct? Use the current ticket no or smthng else + 
+        uint256 ticket_no = _ticketContract.mint(msg.sender);
+        ownedTickets[lottery_no][msg.sender].push(ticket_no);
+        lotteryTickets[lottery_no].push(ticket_no);
+        ticketNoTickets[ticket_no] = Ticket({owner: msg.sender, lottery_no: lottery_no, random_hash: hash_rnd_number, status: Status.BOUGHT});
         totalMoneyInLotteries[lottery_no] += price;
-        current_ticket_no += 1;
     }
 
     function collectTicketRefund(uint ticket_no) public {
         uint lottery_no = getLotteryNo(block.timestamp);
+        require(ticketNoTickets[ticket_no].status == Status.BOUGHT, "Can not refund ticket");
         totalMoneyInLotteries[lottery_no] -= price/2;
     }
 
     function revealRndNumber(uint ticketno, uint rnd_number) public {
         require(ticketno == uint(sha256(abi.encode(rnd_number, msg.sender))), "Reveal");
+        uint lottery_no = getLotteryNo(block.timestamp);
+        Ticket memory ticket = ticketNoTickets[ticketno];
+        ticket.status = Status.REVEALED;
+        revealedTickets[lottery_no].push(ticketno);
+        xorOfLotteries[lottery_no] ^= rnd_number;
     }
     
     function getLastOwnedTicketNo(uint lottery_no) public view returns(uint,uint8 status) {
         uint len = ownedTickets[lottery_no][msg.sender].length;
         uint ticket_no = ownedTickets[lottery_no][msg.sender][len-1];
-        return (ticket_no, uint8(ticketNoTickets[lottery_no][ticket_no].status));
+        return (ticket_no, uint8(ticketNoTickets[ticket_no].status));
     }
 
     function getIthOwnedTicketNo(uint i,uint lottery_no) public view returns(uint,uint8 status) {
-        return (ownedTickets[lottery_no][msg.sender][i], 0);
+        require(i < ownedTickets[lottery_no][msg.sender].length, "Out of bounds");
+        uint ticket_no = ownedTickets[lottery_no][msg.sender][i];
+        return (ticket_no, uint8(ticketNoTickets[ticket_no].status));
     }
 
     function checkIfTicketWon(uint ticket_no) public view returns (uint amount) {
+        
         uint lottery_no = getLotteryNo(block.timestamp);
+        require(ticketNoTickets[ticket_no].lottery_no < lottery_no, "Lottery is not finished");
         require(numberOfTotalWinner(lottery_no)>0);
         
-        uint xored = xorAll(lottery_no);
+        uint xored = xorOfLotteries[lottery_no];
         bytes32 hashed = sha256(abi.encodePacked(xored));
+
         for(uint i = 1;  i < numberOfTotalWinner(lottery_no); i++){
             hashed = sha256(abi.encodePacked(hashed));
             if(uint(hashed) == ticket_no){
@@ -97,13 +115,15 @@ contract Lottery is ILottery {
     }
 
     function collectTicketPrize(uint ticket_no) public {
+        require(!isInPurchase(), "In purchase");
         uint amount = checkIfTicketWon(ticket_no);
+        require(amount > 0, "Did not win");
         _tokenContract.transferFrom(address(this), msg.sender, amount);
     }
 
     function getIthWinningTicket(uint i, uint lottery_no) public view returns (uint ticket_no,uint amount) {
         require(i<numberOfTotalWinner(lottery_no));
-        uint xored = xorAll(lottery_no);
+        uint xored = xorOfLotteries[lottery_no];
         bytes32 hashed = sha256(abi.encodePacked(xored));
         amount = findIthPrizeOfLottery(lottery_no, i);
         for(uint index = 1; index < i; i++){
@@ -125,13 +145,17 @@ contract Lottery is ILottery {
         return unixtimeinseconds/60/60/24/7;
     }
 
+    /*
     function xorAll(uint lottery_no) private view returns (uint xored){
         xored = 0;
         for(uint i = 0; i<lotteryTickets[lottery_no].length; i++) {
-            xored ^= lotteryTickets[lottery_no][i];
+            uint ticketNo = lotteryTickets[lottery_no][i];
+            // xored ^= ;
         }
         return xored;
     }
+    */
+    
 
     function findIthPrizeOfLottery(uint lottery_no, uint i) private view returns (uint){ 
         uint totalMoney = getTotalLotteryMoneyCollected(lottery_no);
@@ -151,5 +175,25 @@ contract Lottery is ILottery {
             i+=1;
         }
         return(i);
+    }
+
+    function isInPurchase() private view returns (bool) {
+        return block.timestamp - (getLotteryNo(block.timestamp) * 7 days) <= 4 days;
+    }
+
+    function fillWinningTickets(uint lottery_no) private {
+        
+        require(numberOfTotalWinner(lottery_no)>0);
+        if(winningTickets[lottery_no].length == 0){
+
+            uint xored = xorOfLotteries[lottery_no];    
+            bytes32 hashed = sha256(abi.encodePacked(xored));
+            winningTickets[lottery_no].push(uint(hashed));
+            for(uint i = 1;  i < numberOfTotalWinner(lottery_no); i++){
+                hashed = sha256(abi.encodePacked(hashed));
+                winningTickets[lottery_no].push(uint(hashed));
+            }
+        }
+        
     }
 }
