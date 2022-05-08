df3b8995fe9d69eb87598cff138e0d49248ab46e
Kadir Elmacı <abdulkadirelmaci@gmail.com>
2022-05-09 01:11:40 +0300

    Add tests

--- Homework 1/test/lottery.js
+++ Homework 1/test/lottery.js
@@ -1,16 +1,64 @@
-const lottery = artifacts.require("Lottery");
-
+const Lottery = artifacts.require("Lottery");
+const TLToken = artifacts.require("TLToken")
+const PREFIX = "Returned error: VM Exception while processing transaction: ";
 
 contract("Lottery", accounts => {
-    it("should put 10000 MetaCoin in the first account", () =>
-      MetaCoin.deployed()
-        .then(instance => instance.getBalance.call(accounts[0]))
+    it("should start at lottery no 0", () =>
+      Lottery.deployed()
+        .then(instance => instance.getLotteryNo(Math.floor(Date.now() / 1000)))
+        .then(lotteryNo => {
+          assert.equal(
+            lotteryNo.valueOf(),
+            0,
+            "Lottery no is not 0"
+          );
+        })
+    );
+
+    it("should deposit 10 TL", () => {
+      let tlTokenInstance;
+      let lotteryInstance;
+      let account = accounts[0];
+      TLToken.deployed()
+        .then(instance => tlTokenInstance = instance);
+      
+      Lottery.deployed()
+        .then(instance => {
+          lotteryInstance = instance;
+          tlTokenInstance.approve(lotteryInstance.address, 10 ** 10, { from: account })
+        })
+        .then(() => lotteryInstance.depositTL(10 ** 9))
+        .then(() => tlTokenInstance.balanceOf(lotteryInstance.address))
         .then(balance => {
           assert.equal(
             balance.valueOf(),
-            10000,
-            "10000 wasn't in the first account"
-          );
-        }));
-    }
-);
+            10 ** 9,
+            "TL is not deposited"
+          )
+        });
+    });
+
+    it("should throw insufficient funds", () => {
+      let tlTokenInstance;
+      let lotteryInstance;
+      let account = accounts[1];
+      TLToken.deployed()
+        .then(instance => tlTokenInstance = instance);
+      
+      Lottery.deployed()
+        .then(instance => {
+          lotteryInstance = instance;
+          tlTokenInstance.approve(lotteryInstance.address, 10, { from: account })
+        })
+        .then(() => {
+          lotteryInstance.depositTL(1000, {from: account});  
+          // try {
+          //   lotteryInstance.depositTL(1000);  
+          // } catch (error) {
+          //   //assert(error, "Expected an error but did not get one");
+          //   assert(error.message.startsWith("asdhjghjgadshjgasdhgjadshj"), "resutl" + error.message )
+          // }
+        })
+    });
+  
+});
--- Homework 1/test/tltoken.js
+++ Homework 1/test/tltoken.js
@@ -3,13 +3,26 @@ const TLToken = artifacts.require("TLToken");
 contract("TLToken", accounts => {
     it("should put 10^25 TL in the first account", () =>
       TLToken.deployed()
-        .then(instance => instance.getBalance.call(accounts[0]))
+        .then(instance => instance.balanceOf(accounts[0]))
         .then(balance => {
           assert.equal(
             balance.valueOf(),
-            10 ** 25,
-            "10^25 wasn't in the first account"
+            10 ** 43,
+            "10^25 TL wasn't in the first account"
           );
-        }));
-    }
-);
+        })
+    );
+
+    it("should total supply be 20^25 TL", () =>
+      TLToken.deployed()
+        .then(instance => instance.totalSupply())
+        .then(balance => {
+          assert.equal(
+            balance.valueOf(),
+            2 * 10 ** 43,
+            "20^25 TL wasn't in the first account"
+          );
+        })
+    );
+  }
+)
